310a036a417d9d1128205a9e7ac0aaed133f3dbe
Егор <katagarov@yandex.ru>
2018-02-11 19:01:17 +0300

    Создание config.py
    Создание конфигурационного файла

---
+++ config.py
@@ -0,0 +1,8 @@
+# ID приложения
+CLIENT_ID = 0000000
+# Здесь указаны права доступа
+SCOPE = "friends,photos,video,status,messages,wall,offline,docs,groups"
+# Номер телефона или email от профиля бота
+EMAIL = ""
+# Пароль от профиля бота
+PASSWORD = ""
