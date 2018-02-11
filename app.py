ae42b2de76371c2f7f2831d1f2f0caa8b43d49fd
Егор <katagarov@yandex.ru>
2018-02-11 18:58:06 +0300

    Создание app.py
    Файл где происходит все подключение, он же файл для запуска бота

---
+++ app.py
@@ -0,0 +1,24 @@
+from core import VkApi
+import config
+from commands.speak import cmd as speak
+import logger
+
+bot = VkApi(config.CLIENT_ID, config.EMAIL, config.PASSWORD, config.SCOPE)
+
+logger.log("Подключение команд")
+
+#Тут указать команды
+commands = [speak]
+
+list_commands = []
+
+for cmd in commands:
+    list_commands.append(str(cmd['name']))
+
+logger.log("Были подключены следующий команды: " + str(",".join(list_commands)))
+
+logger.log("Запуск обработки сообщений")
+
+while True:
+    last_message = bot.get_last_messages(10)
+    bot.listen(last_message, commands)
