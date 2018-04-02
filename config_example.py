f96ea165447e06816b5500616d9b205adf433166
Egor Katagarov <katagarov@yandex.ru>
2018-04-02 18:39:33 +0300

    Добавление конфигурационного файла-примера и обновление README

---
+++ config_example.py
@@ -0,0 +1,4 @@
+CLIENT_ID = 0000000  # ID приложения
+SCOPE = "friends,photos,video,status,messages,wall,offline,docs,groups"  # Здесь указаны права доступа
+EMAIL = ""  # Номер телефона или email от профиля бота
+PASSWORD = ""  # Пароль от профиля бота
