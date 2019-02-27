7f626e9ecab404aa96ea8757f9709ebde5d55990
boroda34 <katagarov@yandex.ru>
2019-02-27 22:56:03 +0400

    начало рефакторинга

--- app.py
+++ app.py
@@ -1,26 +1,16 @@
-from core import VkApi
 import config
-import logger
 
-from commands.speak import cmd as speak
-from middleware.read_messages import read_messages
+from vk_api.api import VkApi
+import vk_api.logger as logger
 
-bot = VkApi(config.CLIENT_ID, config.EMAIL, config.PASSWORD, config.SCOPE)
+log = logger.Logger('main')
 
-logger.log("Подключение команд")
+bot = VkApi({
+    'client_id': config.CLIENT_ID,
+    "scope": config.SCOPE,
+    "api_v": config.VERSION_API,
+    "login": config.LOGIN,
+    "password": config.PASSWORD
+})
 
-commands = [speak]  # Тут указать команды
-middlewares = [read_messages]  # Тут указать обработчики
-
-list_commands = []
-
-for cmd in commands:
-    list_commands.append(str(cmd['name']))
-
-logger.log("Были подключены следующий команды: " + str(",".join(list_commands)))
-
-logger.log("Запуск обработки сообщений")
-
-while True:
-    last_message = bot.get_last_messages(10)
-    bot.listen(last_message, commands, middlewares)
+bot.login()
--- config_example.py
+++
@@ -1,4 +0,0 @@
-CLIENT_ID = 0000000  # ID приложения
-SCOPE = "friends,photos,video,status,messages,wall,offline,docs,groups"  # Здесь указаны права доступа
-EMAIL = ""  # Номер телефона или email от профиля бота
-PASSWORD = ""  # Пароль от профиля бота
